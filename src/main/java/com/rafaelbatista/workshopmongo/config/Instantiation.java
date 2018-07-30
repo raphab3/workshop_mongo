0f957d5314ad46fea8c48fc0ae7bc5e44bcc2dea
Rafael <raphab33@hotmail.com>
2018-07-29 21:07:31 -0300

    Referenciando os posts do usuário

--- src/main/java/com/rafaelbatista/workshopmongo/config/Instantiation.java
+++ src/main/java/com/rafaelbatista/workshopmongo/config/Instantiation.java
@@ -41,8 +41,12 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("29/07/2018"), "Post1 um ", "top de linha", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("28/07/2018"), "Post2 um ", "Massa d+", new AuthorDTO(maria));
 		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
-		postRepository.saveAll(Arrays.asList(post1, post2)); 
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
+		 
 		
 		
 	}
